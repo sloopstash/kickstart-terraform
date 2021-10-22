c4cf5da4f7a2f9d93fee2377a6dacc73d6fdf2d4
John Sundarraj <johnsundarraj88@gmail.com>
2021-10-22 11:12:08 +0530

    Included missing variable definitions in Terraform configuration.

--- module/aws/variables.tf
+++ module/aws/variables.tf
@@ -2,6 +2,10 @@ variable "env" {
   type = string
   description = "Environment."
 }
+variable "s3_bucket_prefix" {
+  type = string
+  description = "S3 bucket prefix."
+}
 variable "ssh_public_key" {
   type = string
   description = "SSH public key."
--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "env" {
   type = string
   description = "Environment."
 }
+variable "aws_s3_bucket_prefix" {
+  type = string
+  description = "AWS S3 bucket prefix."
+}
 variable "ssh_public_key" {
   type = string
   description = "SSH public key."
